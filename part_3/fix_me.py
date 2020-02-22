922852ed83e74067080251cdd08885b573146680
De Xing <dzx0010@auburn.edu>
2020-02-21 22:52:35 -0600

    line 62 same indent

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -1,7 +1,7 @@
 #! /bin/bash/python3
 
 # Reading data from the user's input
-a = input("Enter a : ")
+a = input('Enter a : ')
 
 print("You entered", a, "which is a", type(a))
 
@@ -59,7 +59,7 @@ print("Now \"b\" equals", b, "and is a", type(b))
 
 
 total = a + b
- print("a + b =", total, ", which is a", type(total))
+print("a + b =", total, ", which is a", type(total))
 
 difference = a - b
 print("a - b =", difference, ", which is a", type(difference))
